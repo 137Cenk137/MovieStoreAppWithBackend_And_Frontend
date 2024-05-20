162c2abde2886c33691f68b8c26131e4000552ea
Cenk Kıran <cenk33m@gmail.com>
2024-05-20 13:40:11 +0300

    Genre was written

---
+++ MovieStoreApp/WebApi/Entities/Genre.cs
@@ -0,0 +1,8 @@
+public class Genre
+{
+    public int Id { get; set;}
+    public string GenreName { get; set;}
+    public int Customer
+    Id { get; set;}
+    public Customer  Customer { get; set; }
+}
