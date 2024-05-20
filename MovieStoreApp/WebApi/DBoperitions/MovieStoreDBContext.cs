6b2e8d7e9e1582f9a783600d46553363836192f0
Cenk Kıran <cenk33m@gmail.com>
2024-05-20 15:16:38 +0300

    basic db operations was was written

---
+++ MovieStoreApp/WebApi/DBoperitions/MovieStoreDBContext.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+public class MovieStoreDBContext : DbContext
+{
+    public MovieStoreDBContext(DbContextOptions<MovieStoreDBContext> options): base(options)
+    {
+        
+    }
+    public DbSet<BaseEntity> baseEntities{ get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<BaseEntity>().UseTpcMappingStrategy();
+    }
+
+}
