d6729532af5634b83de9c296fbd4014d517ee490
Cenk Kıran <cenk33m@gmail.com>
2024-05-28 20:36:05 +0300

    ActorControllers was written

---
+++ MovieStoreApp/WebApi/Controllers/ActorControllers.cs
@@ -0,0 +1,63 @@
+using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Applications.ActorOperations.Command.CreateActor;
+using WebApi.Applications.ActorOperations.Command.DeleteActor;
+using WebApi.Applications.ActorOperations.Command.UpdateActor;
+using WebApi.Applications.ActorOperations.Queries;
+using WebApi.Applications.MovieOperations.Queries.GetMovie;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ActorControllers : ControllerBase
+{
+   private readonly MovieStoreDBContext _dbContext;
+   private readonly IMapper  _mapper;
+
+   public ActorControllers(MovieStoreDBContext dbContext,IMapper mapper)
+   {
+        _dbContext = dbContext;
+        _mapper = mapper;
+   }
+   [HttpGet]
+   public ActionResult Get()
+   {
+    GetActorQuery  query = new(_dbContext, _mapper);
+    var result = query.Handle();
+    return Ok(result);
+   }
+
+   [HttpPost]
+   public ActionResult Post([FromBody] CreateActorModel Model)
+   {
+        CreateActorCommand  command= new CreateActorCommand(_dbContext, _mapper);
+        command.Model =  Model;
+        CreateActorValidator validations = new();
+        validations.ValidateAndThrow(command);
+        command.Handle();
+
+        return Ok();
+   }
+
+   [HttpDelete("{id}")]
+   public ActionResult Delete(int id)
+   {
+     DeleteActorCommand command = new(_dbContext){DeleteId = id};
+     command.Handle();
+
+     return Ok();
+
+   }
+
+   [HttpPut("{id}")]
+   public ActionResult Put(int id,[FromBody] UpdateActorModel actor)
+   {
+     UpdateActorCommand command = new(_dbContext);
+     command.Model = actor;
+     command.UpdateId = id;
+     command.Handle();
+     return Ok();
+   }
+}
